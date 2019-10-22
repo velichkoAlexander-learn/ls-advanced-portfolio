23dcf3ff7a41b1dd97259b5bf31b10208c441d21
Alexander Velichko <alvi@mail.ua>
2019-10-22 17:06:57 +0300

    vue tasks done

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,34 @@
-console.log('this is skills module');
+import Vue from 'vue';
+
+const skill = {
+    template: "#skill",
+    props: ["skill-name", "skill-percent"]
+};
+
+const skillsRow = {
+    template: "#skills-row",
+    components: {skill},
+    props: ["skill"],
+    mounted() {
+        const circle = this.$refs['const-circle'];
+        const dashArray = parseInt(
+          getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+        );
+        const percent = (dashArray / 100) * (100 - this.skillPercent);
+    
+        circle.style.strokeDashoffset = percent;
+    }
+};
+
+new Vue({
+    el: "#skills-component",
+    template: "#skills-list",
+    components: {skillsRow},
+    data: () => ({
+        skills: {}
+    }),
+    created() {
+        this.skills = require('../data/skills');
+    }
+});
+
