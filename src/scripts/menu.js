7da3b049db25232cc79d29d1394bacd86a2c2ffa
Alexander Velichko <alvi@mail.ua>
2019-10-24 15:52:02 +0300

    closed mobile menu when click on a

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,13 +1,21 @@
 const menuButton = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
-const menuCloseButton =  document.querySelector('.mobile-menu__btn-close');
+const menuCloseButton = document.querySelector('.mobile-menu__btn-close');
+const menuMobileItem = document.querySelector('.mobile-menu__nav .nav__list');
 
 const toggleMobileMenu = () => {
     mobileMenu.classList.toggle('active');
 };
 
-menuButton.addEventListener('click',toggleMobileMenu);
-menuCloseButton.addEventListener('click',toggleMobileMenu);
+const closeMenuAfterClick = (e) => {
+    if (e.target.classList.contains('nav__link')) {
+        toggleMobileMenu();
+    }
+};
+
+menuButton.addEventListener('click', toggleMobileMenu);
+menuCloseButton.addEventListener('click', toggleMobileMenu);
+menuMobileItem.addEventListener('click', closeMenuAfterClick);
 
 
 
