fbd01444c747821de910a193495b730381fbb4df
Alexander Velichko <alvi@mail.ua>
2019-10-27 15:25:07 +0200

    added works section

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -116,10 +116,65 @@
               .input-wrapper
                 input.input#tag(type="text" name="tag" placeholder=" "  required )
                 label.input-label(for="tag") Добавление тэга
+              .works-form__tags
+                ul.tags
+                  li.tags__item
+                    span HTML
+                    a.tags__link(href="#")
+                  li.tags__item
+                    span CSS
+                    a.tags__link(href="#")
+                  li.tags__item
+                    span Javascript
+                    a.tags__link(href="#")
               .works-form__btns
                 .control-btns
                   button(type="button").control-btn.control-btn--cancel Отмена
                   button(type="button").control-btn.control-btn--save Сохранить
+        .works__list
+          .works__item
+            button(type="button").add-card
+                .add-card__content
+                  .add-card__icon +
+                  .add_card_text Добавить работу
+          .works__item
+            .card
+              .card__hero
+                img.card__hero-image(src="../images/card_hero.jpg" alt="alt")
+                .card__hero-tags
+                  ul.tags
+                    li.tags__item
+                      span HTML
+                    li.tags__item
+                      span CSS
+                    li.tags__item
+                      span Javascript
+              .card__body
+                h2 Сайт школы образования
+                p Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                a(href="//google.com") http://loftschool.ru
+              .card__footer
+                button(type="button").card__btn.card__btn--edit Править
+                button(type="button").card__btn.card__btn--remove Удалить
+          .works__item
+            .card
+              .card__hero
+                img.card__hero-image(src="../images/card_hero.jpg" alt="alt")
+                .card__hero-tags
+                  ul.tags
+                    li.tags__item
+                      span HTML
+                    li.tags__item
+                      span CSS
+                    li.tags__item
+                      span Javascript
+              .card__body
+                h2 Сайт школы образования
+                p Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                a(href="//google.com") http://loftschool.ru
+              .card__footer
+                button(type="button").card__btn.card__btn--edit Править
+                button(type="button").card__btn.card__btn--remove Удалить
 
 
 
