bd025c480d3b6ee1b0e93b2e22db6265943a6cb7
Alexandr Velichko <alvi@mail.ua>
2019-10-29 16:40:38 +0200

    added template for slider vue
    (cherry picked from commit e7714ee6e982acad044f4a46064017209b35faf4)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import "./styles/main.pcss";
-if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
+import './styles/main.pcss';
+if (process.env.NODE_ENV === 'development') {
+	require('file-loader!./index.pug');
 }
 
-
-import "./scripts/parallax";
-import "./scripts/skills";
-import "./scripts/carusel";
-import "./scripts/menu";
+import './scripts/parallax';
+import './scripts/skills';
+import './scripts/carusel';
+import './scripts/menu';
+import './scripts/slider';
