6708f0a6f390dfcf347e13d047b8892240bc4899
Alexander Velichko <alvi@mail.ua>
2019-10-30 09:36:29 +0200

    added slider loop

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,54 +1,81 @@
 import Vue from 'vue';
 
 const SliderTag = {
-	template: '#slider-tag'
+    template: '#slider-tag'
 };
 
 const SliderPreview = {
-	template: '#slider-preview',
-	props: [ 'works', 'currentWork' ],
-	computed: {
-		reverseWorks() {
-			return [ ...this.works ].reverse();
-		}
-	}
+    template: '#slider-preview',
+    props: ['works', 'currentWork'],
+    computed: {
+        reverseWorks() {
+            return [...this.works].reverse();
+        }
+    }
 };
 
-const SliderControlls = {
-	template: '#slider-controlls'
+const SliderControls = {
+    template: '#slider-controls'
 };
 
 const SliderMain = {
-	template: '#slider-main',
-	props: [ 'works', 'currentWork' ],
-	components: {
-		SliderPreview,
-		SliderControlls
-	}
+    template: '#slider-main',
+    props: ['works', 'currentWork','currentIndex'],
+    components: {
+        SliderPreview,
+        SliderControls
+    }
 };
 
 const SliderInfo = {
-	template: '#slider-info',
-	props: [ 'works', 'currentWork' ],
-	components: {
-		SliderTag
-	}
+    template: '#slider-info',
+    props: ['works', 'currentWork'],
+    components: {
+        SliderTag
+    }
 };
 
 new Vue({
-	el: '#slider-component',
-	template: '#slider',
-	components: { SliderInfo, SliderMain },
-	data: () => ({
-		works: [],
-		currentWork: {},
-		currentIndex: 0
-	}),
-	created() {
-		fetch('https://webdev-api.loftschool.com/works/174').then((res) => res.json()).then((data) => {
-			data.map((item) => (item.photo = `https://webdev-api.loftschool.com/${item.photo}`));
-			this.works = data;
-			this.currentWork = this.works[this.currentIndex];
-		});
-	}
+    el: '#slider-component',
+    template: '#slider',
+    components: {SliderInfo, SliderMain},
+    data: () => ({
+        works: [],
+        currentIndex: 0
+    }),
+    created() {
+        fetch('https://webdev-api.loftschool.com/works/174').then((res) => res.json()).then((data) => {
+            data.map((item) => (item.photo = `https://webdev-api.loftschool.com/${item.photo}`));
+            this.works = data;
+        });
+    },
+    watch: {
+        currentIndex(value) {
+            const worksAmount = this.works.length - 1;
+            if (value < 0) {
+                this.currentIndex = worksAmount;
+            }
+            if (value > worksAmount) {
+                this.currentIndex = 0;
+            }
+            
+        }
+        
+        
+    },
+    computed: {
+        currentWork() {
+            return this.works[this.currentIndex];
+        }
+    },
+    methods: {
+        handleSlide(direction) {
+            if (direction === 'next') {
+                this.currentIndex++;
+            }
+            if (direction === 'prev') {
+                this.currentIndex--;
+            }
+        }
+    }
 });
